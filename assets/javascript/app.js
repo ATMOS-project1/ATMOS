6d25b1b070b19cee96ba2c8f5c828e38f4588926
dtroxler21 <dtroxler21@gmail.com>
2018-03-10 10:16:05 -0500

    append station info on table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,14 +25,14 @@
 //------------------//
 
 // Function to grab inputs and get the API information
-$("#").on("click", function(event) {
+$("#submit").on("click", function(event) {
   //Preventing the page from reloading after the button is clicked
   event.preventDefault();
 
   // Grabbing inputs to be stored as variables to be used in the URL
   var state = $("#state").val();
   var connectorType = $("#connector-type").val();
-  var zip = $("#zip").val();
+  var zip = $("#zip").val().trim();
   var chargingLevel = $("#charging-level").val();
 
   // Creating URL using the inputs to search for fuel stations in the API
@@ -51,56 +51,23 @@ $("#").on("click", function(event) {
     var results = response.fuel_stations;
     console.log(results);
 
-    // For loop to store the individual station information as variables and displaying them
+    // For loop to store the individual station information as variables to be displayed
     for (var i = 0; i < results.length; i++) {
-      // Emptying the div holding any prior station information
-      $("#").empty();
+      // Emptying the display div
+      $("tbody").empty();
 
-      // Creating new div for each fuel station
-      var stationDiv = $("<div>");
-      
       // Storing information in variables
       var stationName = results[i].station_name;
       var stationAddress = results[i].street_address + " " + results[i].city + " " + results[i].state + " " + results[i].zip;
       var stationHours = results[i].access_days_time;
       var stationPhone = results[i].station_phone;
-      var cardsAccepted = results[i].cards_accepted;
-      var stationStatus = results[i].status_code;
-      //Need to look over how best to store charging level
-      var chargingLevel = "";
-      var stationAccess = results[i].groups_with_access_code;
-      var connectorTypes = results[i].ev_connector_types;
-      var fuelType = results[i].fuel_type_code;
-      var fillType = results[i].ng_fill_type_code;
 
-      // Creating an element to have the rating displayed
-      var ratingP = $("<p>").text("Rating: " + rating);
+      // Setting a data attribute to store the address to be used in other API
 
-      // Displaying the rating
-      gifDiv.append(ratingP);
 
-      // Creating an image for the gifs
-      var sportImage = $("<img class='gif'>")
-            
-      // Giving image attribute to store still gif
-      sportImage.attr("data-still", results[i].images.original_still.url);
-
-      // Storing the original gif            
-      var originalGIF = results[i].images.original.url;
-      console.log(originalGIF);
-
-      // Giving the image an attribute for the original gif
-      sportImage.attr("data-animate", originalGIF);
-      // console.log(sportImage.attr("date-animate"));
-
-      // Giving the image an attribute for data-state to be used later
-      sportImage.attr("data-state", "still");
-
-      // Displaying the gifs
-      gifDiv.append(sportImage);
-
-      // Putting the gifs at the top of the div
-	    $("#").append(stationDiv);
+      // Displaying the station information in a new table row
+      $("tbody").append("<tr><td>" + stationName + "</td><td>" + stationAddress + "</td><td>" +
+      stationHours + "</td><td>" + stationPhone + "</td></tr>");
     };
   });
 });
