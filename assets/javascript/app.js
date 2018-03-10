8ccbde38fdbf3c14ea5a155e932f36797fe8f25d
dtroxler21 <dtroxler21@gmail.com>
2018-03-10 10:47:45 -0500

    cleaning

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,9 @@
-// // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyCS_LJiMZGK0YtgqxqKgj4eGPD2obBOMpI",
-//     authDomain: "atmos-bcd7a.firebaseapp.com",
-//     databaseURL: "https://atmos-bcd7a.firebaseio.com",
-//     projectId: "atmos-bcd7a",
-//     storageBucket: "atmos-bcd7a.appspot.com",
-//     messagingSenderId: "671205675180"
-//   };
-//   firebase.initializeApp(config);
-
-//  // Create a variable to reference the database.
-// 	var database = firebase.database();
-
-// // Create a variable for current time.
-//   var currentTime = moment();
- 
-// $(document).ready(function(){
-//     $('.parallax').parallax();
-//   });
-
-
 //------------------//
 // All Stations API //
 //------------------//
 
 // Function to grab inputs and get the API information
-$("#submit").on("click", function(event) {
+$(".btn").on("click", function(event) {
   //Preventing the page from reloading after the button is clicked
   event.preventDefault();
 
